213434941a7a6480ab9fa25e13278bfd2649144b
TakundaPraise <takundapraiseg@gmail.com>
2024-05-22 14:07:44 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -112,7 +112,7 @@ if uploaded_file is not None:
 
         # Rescale the forecasts
         rescale_vals = np.load('VISUELLE/normalization_scale.npy')
-        rescaled_forecasts = y_pred.detach().cpu().numpy().flatten()[:12] * rescale_vals
+        rescaled_forecasts = y_pred.detach().cpu().numpy().flatten()[:52] * rescale_vals
 
         # Round the forecasts to whole numbers
         rounded_forecasts = np.round(rescaled_forecasts).astype(int)
@@ -124,15 +124,18 @@ if uploaded_file is not None:
     chart_expander = st.expander("Click to expand", expanded=False)
     with chart_expander:
         st.write('The line chart represents the sales predictions for new fashion products. he y-axis represents the sales predictions.The chart displays a line that indicates the predicted sales over time. The values on the y-axis represent the estimated number of sales for each corresponding time point on the x-axis. The line connects these data points, showing the trend or pattern in the sales predictions. The predictions are then rescaled using normalization values and presented in both the line chart and the table.')
-        month_labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May','Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'  ]
+        week_labels = [f'Week {i+1}' for i in range(52)]
 
         # Create the line chart with updated labels
         fig, ax = plt.subplots(figsize=(12, 6))
         #ax.plot(rescaled_forecasts)
         ax.plot(rounded_forecasts)
-        ax.set_xticks(range(len(month_labels)))
-        ax.set_xticklabels(month_labels)
-        ax.set_xlabel('Month')
+        ax.set_xticks(range(len(week_labels)))
+        ax.set_xticklabels(week_labels, rotation=90)
+        ax.set_xlabel('Week')
+        #ax.set_xticks(range(len(month_labels)))
+        #ax.set_xticklabels(month_labels)
+        #ax.set_xlabel('Week')
         ax.set_ylabel('Sales Predictions')
         st.pyplot(fig)
         #st.line_chart(rescaled_forecasts)
